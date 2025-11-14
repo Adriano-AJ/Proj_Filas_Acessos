ebbff8b94caba56a4ff1a7d8d58221c9393c7c89
adriano <adrianobinan@gmail.com>
2025-11-14 14:58:12 -0300

    feat: Classe Log para registrar os acessos dos usuarios.

---
+++ Proj_Filas_Acessos/Log.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proj_Filas_Acessos
+{
+    public class Log
+    {
+        private DateTime dtAcesso;
+        private Usuario usuario;
+        private bool tipoAcesso;
+
+        public DateTime DtAcesso { get => dtAcesso; }
+        public Usuario Usuario { get => usuario; }
+        public bool TipoAcesso { get => tipoAcesso; }
+
+        public Log(DateTime dtAcesso, Usuario usuario, bool tipoAcesso)
+        {
+            this.dtAcesso = dtAcesso;
+            this.usuario = usuario;
+            this.tipoAcesso = tipoAcesso;
+        }
+    }
+}
