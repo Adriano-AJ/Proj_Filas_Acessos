f1fba6d7456311b4ef8732f852167dd07babf128
adriano <adrianobinan@gmail.com>
2025-11-14 15:59:30 -0300

    feat: Classe Cadastro com metodos para gerenciar usuarios e ambientes.

---
+++ Proj_Filas_Acessos/Cadastro.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proj_Filas_Acessos
+{
+    public class Cadastro
+    {
+        private List<Usuario> usuarios;
+        private List<Ambiente> ambientes;
+
+        public List<Usuario> Usuarios { get => usuarios;}
+        public List<Ambiente> Ambientes { get => ambientes;}
+
+        public Cadastro()
+        {
+            this.usuarios = new List<Usuario>();
+            this.ambientes = new List<Ambiente>();
+        }
+
+        public void adicionarUsuario(Usuario usuario)
+        {
+            usuarios.Add(usuario);
+        }
+
+        public bool removerUsuario(Usuario usuario)
+        {
+            return usuarios.Remove(usuario);
+        }
+
+        public Usuario pesquisarUsuario(Usuario usuario)
+        {
+            return usuarios.Find(u => u.Id == usuario.Id);
+        }
+
+        public void adicionarAmbiente(Ambiente ambiente)
+        {
+            ambientes.Add(ambiente);
+        }
+
+        public bool removerAmbiente(Ambiente ambiente)
+        {
+            return ambientes.Remove(ambiente);
+        }
+
+        public Ambiente pesquisarAmbiente(Ambiente ambiente)
+        {
+            return ambientes.Find(a => a.Id == ambiente.Id);
+        }
+
+        public void upload()
+        {
+
+        }
+
+        public void download()
+        {
+
+        }
+    }
+}
