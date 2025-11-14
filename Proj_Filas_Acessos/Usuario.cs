582f712342f075735c35e278aa50db94f57c7146
adriano <adrianobinan@gmail.com>
2025-11-14 14:45:33 -0300

    feat: Classe Usuario com metodos para gerenciar acesso.

---
+++ Proj_Filas_Acessos/Usuario.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proj_Filas_Acessos
+{
+    public class Usuario
+    {
+        private int id;
+        private string nome;
+        private List<Ambiente> ambientes;
+
+        public int Id { get => id;}
+        public string Nome { get => nome; set => nome = value; }
+        public List<Ambiente> Ambientes { get => ambientes;}
+
+        public Usuario(int id, string nome)
+        {
+            this.id = id;
+            this.nome = nome;
+            this.ambientes = new List<Ambiente>();
+        }
+
+        public bool concederPermissao(Ambiente ambiente)
+        {
+            return false;
+        }
+        public bool revogarPermissao(Ambiente ambiente)
+        {
+            return false;
+        }
+    }
+}
